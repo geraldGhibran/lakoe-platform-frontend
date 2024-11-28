6c956713cb97b5e43a31cb1498d25db83298f0c5
greza741 <satrianieros33@gmail.com>
2024-11-29 04:04:38 +0700

    feat: dasboard and withdraw test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MarketplaceHomePage from '@/features/marketplace/home/pages/home';
+import AdminHomePage from '@/features/admin/home/home';
+import Withdraw from '@/features/admin/withdraw/withdraw';
 
 export const router = createBrowserRouter([
   {
@@ -9,5 +11,14 @@ export const router = createBrowserRouter([
   {
     path: '/marketplace',
     element: <MarketplaceHomePage />,
+  },
+
+  {
+    path: '/admin',
+    element: <AdminHomePage />,
+  },
+  {
+    path: '/admin/withdraw',
+    element: <Withdraw />,
   },
 ]);
