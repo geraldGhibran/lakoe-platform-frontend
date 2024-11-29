4deeedd6df535291d3bed9bb3d906c2484c10e44
Kafi Abdur Rahim <kafiabdurrahim@gmail.com>
2024-11-29 10:40:37 +0700

    feat:adding route products-seller

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import AdminHomePage from '@/features/admin/home/home';
 import Withdraw from '@/features/admin/withdraw/withdraw';
 import DetailProductPage from '@/features/marketplace/home/pages/home';
+import ProductList from '@/features/product/components';
 
 export const router = createBrowserRouter([
   {
@@ -20,5 +21,10 @@ export const router = createBrowserRouter([
   {
     path: '/admin/withdraw',
     element: <Withdraw />,
+  },
+
+  {
+    path: '/products',
+    element: <ProductList />,
   },
 ]);
