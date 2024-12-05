b961fea47fa41ad8a228a207708fcffa2603567e
HP <faisalyulianto26@gmail.com>
2024-12-05 09:28:18 +0700

    feat : add page order-seller

---
+++ src/components/leftbar/index.tsx
@@ -0,0 +1,189 @@
+'use client';
+
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Link,
+  Stack,
+  useDisclosure,
+} from '@chakra-ui/react';
+import {
+  MenuContent,
+  MenuItem,
+  MenuRoot,
+  MenuTrigger,
+} from '@/components/ui/menu';
+import { Icon } from '@iconify/react';
+import { useLocation } from 'react-router-dom';
+
+interface NavLinkProps {
+  children: React.ReactNode;
+  icon: string;
+  path: string;
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const Links = [
+  { name: 'Dashboard', icon: 'lucide:home', path: '/admin' },
+  { name: 'Produk', icon: 'bi:box-fill', path: '/add-product' },
+  { name: 'Pesanan', icon: 'solar:bag-5-outline', path: '/order' },
+];
+
+const NavLink = ({ children, icon, path, isActive, onClick }: NavLinkProps) => {
+  const hoverBg = 'gray.200';
+  const linkColor = 'gray.800';
+  const iconColor = isActive ? 'blue' : linkColor;
+
+  return (
+    <Link
+      as="a"
+      display="flex"
+      alignItems="center"
+      px={20}
+      py={5}
+      rounded={'md'}
+      href={path}
+      onClick={onClick}
+      _hover={{
+        textDecoration: 'none',
+        bg: hoverBg,
+      }}
+      color={isActive ? 'blue.500' : linkColor}
+    >
+      <Icon
+        icon={icon}
+        width="24"
+        height="24"
+        style={{
+          marginRight: '12px',
+          color: iconColor,
+        }}
+      />
+      {children}
+    </Link>
+  );
+};
+
+export default function SideBar() {
+  const { open, onOpen, onClose } = useDisclosure();
+  const location = useLocation();
+
+  return (
+    <Box px={4} w={'100%'}>
+      <Flex h={'50vh'} justifyContent={'space-between'}>
+        <IconButton
+          aria-label={'Open Menu'}
+          display={{ md: 'none' }}
+          onClick={open ? onClose : onOpen}
+        />
+        <HStack alignItems={'center'}>
+          <HStack as={'nav'} display={{ base: 'none', md: 'flex' }}>
+            <Flex direction={'column'}>
+              {Links.map((link) => (
+                <NavLink
+                  key={link.name}
+                  icon={link.icon}
+                  path={link.path}
+                  isActive={location.pathname === link.path}
+                  onClick={() => {}}
+                >
+                  {link.name}
+                </NavLink>
+              ))}
+              <MenuRoot>
+                <MenuTrigger asChild>
+                  <Link
+                    display="flex"
+                    alignItems="center"
+                    px={20}
+                    py={5}
+                    rounded={'md'}
+                    _hover={{
+                      bg: 'gray.200',
+                    }}
+                    color={
+                      location.pathname.startsWith('/settings')
+                        ? 'blue.500'
+                        : 'gray.800'
+                    }
+                  >
+                    <Icon
+                      icon="mdi:cog"
+                      width="24"
+                      height="24"
+                      style={{
+                        marginRight: '12px',
+                        color: location.pathname.startsWith('/settings')
+                          ? 'blue'
+                          : 'gray.800',
+                      }}
+                    />
+                    Pengaturan{' '}
+                    <Icon
+                      style={{
+                        marginLeft: '100px',
+                        width: '24px',
+                        height: '24px',
+                      }}
+                      icon="gridicons:dropdown"
+                    />
+                  </Link>
+                </MenuTrigger>
+                <MenuContent width={400}>
+                  <Box pl={2} ml={14}>
+                    <MenuItem value="profile">
+                      <Icon icon="ph:dot-fill" width="30" height="30" />
+                      <Box fontSize={'15px'}>Atur Toko</Box>
+                    </MenuItem>
+                  </Box>
+                  <Box pl={2} ml={14}>
+                    <MenuItem value="account">
+                      <Icon icon="ph:dot-fill" width="30" height="30" />
+                      <Box fontSize={'15px'}>Pengiriman</Box>
+                    </MenuItem>
+                  </Box>
+                  <Box pl={2} ml={14}>
+                    <MenuItem value="security">
+                      <Icon icon="ph:dot-fill" width="30" height="30" />
+                      <Box fontSize={'15px'}>Metode Pembayaran</Box>
+                    </MenuItem>
+                  </Box>
+                </MenuContent>
+              </MenuRoot>
+            </Flex>
+          </HStack>
+        </HStack>
+      </Flex>
+      {open && (
+        <Box pb={4} display={{ md: 'none' }}>
+          <Stack as={'nav'}>
+            {Links.map((link) => (
+              <NavLink
+                key={link.name}
+                icon={link.icon}
+                path={link.path}
+                isActive={location.pathname === link.path}
+                onClick={() => {}}
+              >
+                {link.name}
+              </NavLink>
+            ))}
+          </Stack>
+        </Box>
+      )}
+      <Box mt={'340px'}>
+        <NavLink
+          icon="gg:profile"
+          path="/"
+          isActive={location.pathname === '/profile'}
+          onClick={() => {}}
+        >
+          Profile
+        </NavLink>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/features/order/component/headerMenu/index.tsx
@@ -0,0 +1,82 @@
+import {
+  Box,
+  HStack,
+  Input,
+  InputAddon,
+  Group,
+  createListCollection,
+} from '@chakra-ui/react';
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from '@/components/ui/select';
+import { Checkbox } from '@/components/ui/checkbox';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { Field } from '@/components/ui/field';
+
+export default function HeaderMenu() {
+  const frameworks = createListCollection({
+    items: [
+      { label: 'React.js', value: 'react' },
+      { label: 'Vue.js', value: 'vue' },
+      { label: 'Angular', value: 'angular' },
+      { label: 'Svelte', value: 'svelte' },
+    ],
+  });
+  const [checked, setChecked] = useState(false);
+  return (
+    <Box w={'100%'} bgColor={'white'} mt={'-39px'} py={5}>
+      <HStack direction={'row'} gap={5}>
+        <Group attached w={'35%'}>
+          <InputAddon bgColor={'transparent'} borderRight={'none'}>
+            <Icon
+              icon="lets-icons:file-dock-search-light"
+              width={'20px'}
+              height={'20px'}
+            />
+          </InputAddon>
+          <Input borderLeft={'none'} placeholder="Cari pesanan" />
+        </Group>
+        <Field w={'35%'}>
+          <SelectRoot collection={frameworks}>
+            <SelectTrigger>
+              <SelectValueText placeholder="Kurir" />
+            </SelectTrigger>
+            <SelectContent>
+              {frameworks.items.map((movie) => (
+                <SelectItem item={movie} key={movie.value}>
+                  <Checkbox
+                    checked={checked}
+                    onCheckedChange={(e) => setChecked(!!e.checked)}
+                  >
+                    {movie.label}
+                  </Checkbox>
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </SelectRoot>
+        </Field>
+        <Field w={'35%'}>
+          <SelectRoot collection={frameworks}>
+            <SelectTrigger>
+              <SelectValueText placeholder="Urutkan" />
+            </SelectTrigger>
+            <SelectContent>
+              {frameworks.items.map((movie) => (
+                <Checkbox>
+                  <SelectItem item={movie} key={movie.value}>
+                    {movie.label}
+                  </SelectItem>
+                </Checkbox>
+              ))}
+            </SelectContent>
+          </SelectRoot>
+        </Field>
+      </HStack>
+    </Box>
+  );
+}
