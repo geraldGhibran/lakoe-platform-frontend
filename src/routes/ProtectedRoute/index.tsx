5ceddde04f6bb002869edf8fdfe529c55e3f1755
Gerald Ghibran <gerald.gunasha@gmail.com>
2024-12-09 01:21:42 +0700

    Fix:bug-fixing-admin-auth-route

---
+++ src/routes/ProtectedRoute/index.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '@/store/auth';
+import Cookies from 'js-cookie';
+
+export function ProtectedRoute() {
+  const { user, token } = useAuthStore();
+  const cookieToken = Cookies.get('token');
+  const cookieUser = Cookies.get('user');
+
+  if (!user || !token || !cookieToken || !cookieUser) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
