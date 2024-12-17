3187109629f87ad0b859206486d67b70ffbfa746
febrian syahroni <febrian.fs716@gmail.com>
2024-12-17 23:53:57 +0700

    feat: add switch button

--- src/features/seller/pengiriman/index.tsx
+++ src/features/seller/pengiriman/index.tsx
@@ -6,10 +6,84 @@ import {
   DialogRoot,
   DialogTitle,
 } from '@/components/ui/dialog';
-import { Radio, RadioGroup } from '@/components/ui/radio';
+import { Switch } from '@/components/ui/switch';
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
+import { useState } from 'react';
+
+const switchOptions = [
+  {
+    id: '1',
+    label: 'J&T EZ',
+    description: 'Tersedia untuk COD',
+    image:
+      'https://1.bp.blogspot.com/-awkmdr1rWGI/YILHglBLkFI/AAAAAAAAIVw/lvFK6WSrOo0ki_-FU80DVNtDKR6eDwnWgCLcBGAsYHQ/s16000/jnt.png',
+  },
+  {
+    id: '2',
+    label: 'AnterAja',
+    description: '',
+    image:
+      'https://1.bp.blogspot.com/-G2AH2_9Jhl0/YZgdJAJJqDI/AAAAAAAATh4/U9V2T2vsoNI5gnaiEtYnFcGyiK-dqIYJwCLcBGAsYHQ/s320/Anteraja.png',
+  },
+  {
+    id: '3',
+    label: 'JNE',
+    description: 'Tersedia untuk COD',
+    image: 'https://d290ny10omyv12.cloudfront.net/images/jne-large.png',
+  },
+];
+
+const SwitchItem = ({
+  id,
+  label,
+  description,
+  image,
+  isChecked,
+  onChange,
+}: {
+  id: string;
+  label: string;
+  description?: string;
+  image: string;
+  isChecked: boolean;
+  onChange: (id: string) => void;
+}) => (
+  <Flex
+    _hover={{ bgColor: 'blue.300' }}
+    rounded="md"
+    justify="space-between"
+    padding="20px"
+    alignItems="center"
+  >
+    <Flex alignItems="center">
+      <Image width="100px" src={image} alt={label} />
+      <Text fontWeight="medium" fontSize="20px" ml="10px">
+        {label}
+      </Text>
+    </Flex>
+    {description && <Text color="gray">{description}</Text>}
+    <Switch
+      colorPalette="blue"
+      checked={isChecked}
+      onChange={() => onChange(id)}
+    />
+  </Flex>
+);
 
 export default function PengirimanSeller() {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+  const handleSwitchChange = (id: string) => {
+    setSelectedValues((prevSelected) => {
+      const updatedValues = prevSelected.includes(id)
+        ? prevSelected.filter((value) => value !== id)
+        : [...prevSelected, id];
+
+      console.log('Selected Values:', updatedValues);
+      return updatedValues;
+    });
+  };
+
   return (
     <Box bgColor="white">
       <DialogRoot placement="center" motionPreset="slide-in-bottom">
@@ -32,64 +106,19 @@ export default function PengirimanSeller() {
             </Box>
             {/* Radio */}
 
-            <RadioGroup
-              display="flex"
-              flexDir="column"
-              defaultValue="1"
-              variant="subtle"
-              colorPalette="yellow"
-            >
-              <Radio
-                _hover={{ bgColor: 'blue.300' }}
-                rounded="md"
-                padding="20px"
-                value="1"
-              >
-                <Flex alignItems="center">
-                  <Image
-                    width="100px"
-                    src="https://1.bp.blogspot.com/-awkmdr1rWGI/YILHglBLkFI/AAAAAAAAIVw/lvFK6WSrOo0ki_-FU80DVNtDKR6eDwnWgCLcBGAsYHQ/s16000/jnt.png"
-                  />
-                  <Text fontWeight="medium" fontSize="20px">
-                    J&T EZ
-                  </Text>
-                </Flex>
-                <Text color="gray">Tersedia untuk COD</Text>
-              </Radio>
-              <Radio
-                _hover={{ bgColor: 'blue.300' }}
-                rounded="md"
-                padding="20px"
-                value="2"
-              >
-                <Flex alignItems="center">
-                  <Image
-                    width="100px"
-                    src="https://1.bp.blogspot.com/-G2AH2_9Jhl0/YZgdJAJJqDI/AAAAAAAATh4/U9V2T2vsoNI5gnaiEtYnFcGyiK-dqIYJwCLcBGAsYHQ/s320/Anteraja.png"
-                  />
-                  <Text fontWeight="medium" fontSize="20px">
-                    AnterAja
-                  </Text>
-                </Flex>
-              </Radio>
-              <Radio
-                _hover={{ bgColor: 'blue.300' }}
-                rounded="md"
-                padding="20px"
-                value="3"
-              >
-                <Flex alignItems="center">
-                  <Image
-                    width="100px"
-                    src="https://d290ny10omyv12.cloudfront.net/images/jne-large.png"
-                  />
-                  <Text fontWeight="medium" fontSize="20px">
-                    JNE
-                  </Text>
-                </Flex>
-                <Text color="gray">Tersedia untuk COD</Text>
-              </Radio>
-            </RadioGroup>
+            <Box display="flex" flexDir="column" colorScheme="yellow">
+              {switchOptions.map((option) => (
+                <SwitchItem
+                  key={option.id}
+                  id={option.id}
+                  label={option.label}
+                  description={option.description}
+                  image={option.image}
+                  isChecked={selectedValues.includes(option.id)}
+                  onChange={handleSwitchChange}
+                />
+              ))}
+            </Box>
           </Flex>
         </DialogBody>
 
